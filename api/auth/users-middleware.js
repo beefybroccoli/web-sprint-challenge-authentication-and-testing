0fef8b69620aca7e7d35827da6988fd079e1d921
beefybroccoli <tedkimemail@yahoo.com>
2021-12-16 17:44:39 -0800

    ...

--- api/auth/users-middleware.js
+++ api/auth/users-middleware.js
@@ -1,22 +1,22 @@
+const { BCRYPT_ROUND} = require("../../env");
+
 async function verifyUsernamePassword ( req, res, next){
     
     // 3- On FAILED registration due to `username` or `password` missing from the request body, the response body should include a string exactly as follows: "username and password required".
-
     
     next();
 
 }
 
 async function verifyUniqueFreeUsername(req, res, next){
-
         
-        // 4- On FAILED registration due to the `username` being taken, the response body should include a string exactly as follows: "username taken".
+    // 4- On FAILED registration due to the `username` being taken, the response body should include a string exactly as follows: "username taken".
 
-        // 1- In order to register a new account the client must provide `username` and `password`:
-//       {
-//         "username": "Captain Marvel", // must not exist already in the `users` table
-//         "password": "foobar"          // needs to be hashed before it's saved
-//       }
+    // 1- In order to register a new account the client must provide `username` and `password`:
+    //       {
+    //         "username": "Captain Marvel", // must not exist already in the `users` table
+    //         "password": "foobar"          // needs to be hashed before it's saved
+    //       }
 
     next();
 }
@@ -24,6 +24,7 @@ async function verifyUniqueFreeUsername(req, res, next){
 async function hashPassword (req, res, next){
 
     // IMPLEMENT -  You are welcome to build additional middlewares to help with the endpoint's functionality. DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+    //BCRYPT_ROUND
 
     next();
 }
--- env.js
+++ env.js
@@ -1,3 +1,5 @@
 module.exports = {
     SERVER_PORT : 9000,
+    BCRYPT_ROUND : 8,
+    SECRET : "SUPER MAN LIVE"
 };
