10a922fe12ecfcd49960de8205cdd24d3ed3956e
beefybroccoli <tedkimemail@yahoo.com>
2021-12-16 17:21:57 -0800

    ...

---
+++ data/seeds/01-users-table.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex) {
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {id: 1, username:'user1', password:'password1'},
+        {id: 2, username:'user2', password:'password2'},
+        {id: 3, username:'user3', password:'password3'},
+        {id: 4, username:'user4', password:'password4'},
+        {id: 5, username:'user5', password:'password5'},
+      ]);
+    });
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('./api/server.js');
+const { SERVER_PORT} = require("./env");
 
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || SERVER_PORT;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
