06a018b9a8cc756f834ac2d83c7a876869fb7526
beefybroccoli <tedkimemail@yahoo.com>
2021-12-17 04:05:10 -0800

    ...

--- api/server.test.js
+++ api/server.test.js
@@ -2,35 +2,35 @@ const request = require("supertest");
 const app = require("../api/server");
 const db = require("../data/dbConfig");
 
-beforeAll(()=>{
-  db.migrate.latest();
+beforeAll( async()=>{
+  await db.migrate.latest();
 })
 
-beforeEach(()=>{
-  db.migrate.seed();
+afterAll( async ()=>{
+  await db.migrate.rollback();
 })
 
 describe('[1] - test /api/auth/register endpoint', ()=>{
-  test("[1] - On SUCCESSFUL registration,  the response body should have `id`, `username` and `password`", ()=>{
-    // {
-    //   "id": 1,
-    //   "username": "Captain Marvel",
-    //   "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-    // }', 
-  
-
+  test("[1-1] - On SUCCESSFUL registration,  the response body should have `id`, `username` and `password`", async ()=>{
+    const res = await request(app).post("/api/auth/register").send({username:"tomtom", password:"tomtom"});
+    expect(res.body.username).toBe("tomtom");
+    expect(res.body).toHaveProperty('id');
+    expect(res.body).toHaveProperty('password');
   })
-  test('[1] - On FAILED registration due to `username` or `password` missing from the request body, the response body should include a string exactly as follows: "username and password required".', ()=>{
-
+  test('[1-2] - On FAILED registration due to `username` missing from the request body, the response body should include a string exactly as follows: "username and password required".', async ()=>{
+    const res = await request(app).post("/api/auth/register").send({password:"tomtom"});
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBe("username and password required");
   })
-  test('[1] - On FAILED registration due to the `username` being taken, the response body should include a string exactly as follows: "username taken".', ()=>{
-
+  test('[1-3] - On FAILED registration due to `password` missing from the request body, the response body should include a string exactly as follows: "username and password required".', async ()=>{
+    const res = await request(app).post("/api/auth/register").send({username:"tomtom"});
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBe("username and password required");
   })
-  test('[1] - ', ()=>{
-
-  })
-  test('[1] - ', ()=>{
-
+  test('[1-4] - On FAILED registration due to the `username` being taken, the response body should include a string exactly as follows: "username taken".', async ()=>{
+    const res = await request(app).post("/api/auth/register").send({username:"tomtom", password:"tomtom"});
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBe("username taken");
   })
 })
 
@@ -49,12 +49,6 @@ describe('[1] - test /api/auth/login endpoint', ()=>{
   test('[1] - On FAILED login due to `username` not existing in the db, or `password` being incorrect,  the response body should include a string exactly as follows: "invalid credentials', ()=>{
 
   })
-  test('[1] - ', ()=>{
-
-  })
-  test('[1] - ', ()=>{
-
-  })
 })
 
 describe('[1] - test /apijokes endpoint', ()=>{
@@ -68,9 +62,6 @@ describe('[1] - test /apijokes endpoint', ()=>{
 
   })
   test('[1] - On invalid or expired token in the Authorization header, the response body should include a string exactly as follows: "token invalid".', ()=>{
-
-  })
-  test('[1] - ', ()=>{
 
   })
 })
